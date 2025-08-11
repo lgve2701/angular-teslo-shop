1114c255a8ee0fa16fb0ff608b1523cf66b8aee2
lgve2701 <luisgabriel.vasquezescobar@gmail.com>
2025-08-11 15:04:24 -0600

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    baseUrl: 'http://localhost:3000/api',
-    imgUrlBase: 'http://localhost:3000/api/files/product/',
-    prodUrlBase: 'http://localhost:3000/api/products/',
+    baseUrl: 'https://nest-teslo-shop-a6y2.onrender.com/api', //'http://localhost:3000/api',
+    imgUrlBase: 'https://nest-teslo-shop-a6y2.onrender.com/api/files/product/', //'http://localhost:3000/api/files/product/',
+    prodUrlBase: 'https://nest-teslo-shop-a6y2.onrender.com/api/products/' //''http://localhost:3000/api/products/',
 };
+
